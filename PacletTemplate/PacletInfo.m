141d3467b380cfb4e65036644101c4f270f2d1c7
Arnoud Buzing <arnoudb@wolfram.com>
2017-11-14 08:54:38 -0600

    updates

---
+++ PacletTemplate/PacletInfo.m
@@ -0,0 +1,12 @@
+Paclet[
+  Name -> "PacletTemplate",
+  Description -> "PacletTemplate is a template project for creating a new paclet",
+  Creator -> "Arnoud Buzing",
+  Publisher -> "Wolfram Research",
+  Version -> "0.0.1",
+  MathematicaVersion -> "11.2+",
+  Extensions -> {
+   { "Documentation", Language -> "English"},
+   { "Application", Context -> "PacletTemplate`" }
+  }
+]
---
+++ PacletTemplate/PacletTemplate.wl
@@ -0,0 +1,11 @@
+BeginPackage["PacletTemplate`"]
+
+PacletTemplate::usage = "PacletTemplate[] is a sample function"
+
+Begin["`Private`"]
+
+PacletTemplate[] := "Sample function"
+
+End[]
+
+EndPackage[]
